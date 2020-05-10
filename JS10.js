c9f5c39cc503384c64c316fe8969248a2a86bcb7
Annzzz142 <mazmrono93@gmail.com>
2020-05-10 15:25:53 +0530

    Assignment 4 on JavaScript

---
+++ JS10.js
@@ -0,0 +1,10 @@
+       //Q8: Star pattern
+
+        for(var i=0;i<=3;i++)
+        {
+            for(var j=0;j<=i;j++)
+            {
+                document.write("*");
+            }
+            document.write("<br/>");
+        }
---
+++ JS9.js
@@ -0,0 +1,10 @@
+//Q9: JavaScript Pattern
+
+let str= "Javascript";
+let cache="";
+
+for(let i of str){
+    cache +=i;
+    document.writeln(cache);
+    document.write("<br/>");
+}
